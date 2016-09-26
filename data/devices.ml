97bacd168730f9bf7b2257b67748569f14161486
xapantu <xapantu@xaptop>
2016-09-26 19:39:46 +0200

    Fix all devices list

--- data/devices.ml
+++ data/devices.ml
@@ -1,19 +1,20 @@
 open Eliom_shared.React
 
 module Devices = struct
-	type device = string
+  type device = string
 
-	let list_devices () =
-		["xaptop"]
+  let list_devices () =
+    ["xaptop"]
 
-	let new_device, send_device = React.E.create ()
+  let new_device, send_device = React.E.create ()
 
-	let () = send_device "xaptop"
+  let all_devices = React.S.accum (React.E.map (fun a l -> a::l) new_device) []
 
-	let all_devices, send_device = React.S.create ["testlolo"]
-	let () = send_device ["testttt"]
+  let () = send_device "xaptop"
 
-	let name n = n
+  let () = send_device "testttt"
 
-	let new_device n = send_device (n :: React.S.value all_devices)
+  let name n = n
+
+  let new_device n = send_device n
 end
